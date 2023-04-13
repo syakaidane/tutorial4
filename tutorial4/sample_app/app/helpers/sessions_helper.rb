6489f04f47c40f4763f5d0e0c18999f74e5e670b
syakaidane <121926213+syakaidane@users.noreply.github.com>
2023-04-13 04:10:30 +0000

    chapter9

--- tutorial4/sample_app/app/helpers/sessions_helper.rb
+++ tutorial4/sample_app/app/helpers/sessions_helper.rb
@@ -44,4 +44,18 @@ module SessionsHelper
     @current_user = nil
   end
   
+  # 記憶トークンcookieに対応するユーザーを返す
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      raise       # テストがパスすれば、この部分がテストされていないことがわかる
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+  
 end
